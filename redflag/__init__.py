2482cf87b7a743f0db1b5055cf34cb2c97c9568b
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-07-25 20:44:36 +0100

    initial test fix

--- redflag/__init__.py
+++ redflag/__init__.py
@@ -1,2 +1,15 @@
 # this is what is imported with import * from redflag
 __all__ = ()
+
+
+class Game:
+    def __init__(self):
+        self.players = []
+        for _ in range(4):
+            player = Player()
+            self.players.append(player)
+
+
+class Player:
+    def __init__(self):
+        pass
--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -1,5 +1,11 @@
 import redflag
 
+
 class TestDummy:
     def test_dummy(self):
         pass
+
+
+class TestGame:
+    def test_game(self):
+        redflag.Game()
